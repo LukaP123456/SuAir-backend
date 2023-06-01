072d038d050d9ca82b4dcc371424fe9eb34a61e5
LukaP123456 <luka.prcic@icbtech.rs>
2023-06-01 14:38:49 +0200

    generate csv from data is possible, routes updated

--- app/Controllers/AQIController.js
+++ app/Controllers/AQIController.js
@@ -1,8 +1,31 @@
 const AQdata = require('../Models/AQdata')
+const {AsyncParser} = require('@json2csv/node');
+const fs = require('fs');
+
+async function generateCSV(aqData) {
+    const fields = [
+        {label: 'AQI Value', value: 'pollution.aqius'},
+        {label: 'Main US pollutant', value: 'pollution.mainus'},
+        {label: 'Temperature', value: 'weather.tp'},
+        {label: 'Time of measurement', value: 'time'},
+        {label: 'Air temperature in celsius', value: 'weather.tp'},
+        {label: 'Air pressure', value: 'weather.pr'},
+        {label: 'Air humidity', value: 'weather.hu'},
+        {label: 'Wind speed', value: 'weather.ws'},
+        {label: 'Wind direction', value: 'weather.wd'},
+    ];
+    const parser = new AsyncParser({fields});
+    const data = await parser.parse(aqData).promise();
+    fs.writeFileSync('data.csv', data);
+}
 
 const getAll = async (req, res) => {
     try {
-        const aqData = await AQdata.find({})
+        const generate = req.body.genertateCSV === 'true';
+        const aqData = await AQdata.find({});
+        if (generate) {
+            await generateCSV(aqData);
+        }
         res.send(aqData)
     } catch (error) {
         console.log(error)
@@ -11,12 +34,16 @@ const getAll = async (req, res) => {
 const xofAllTime = async (req, res) => {
     try {
         const worst = req.body.worst === 'true';
+        const generate = req.body.genertateCSV === 'true';
         AQdata.find()
             .sort(worst ? '-pollution.aqius' : 'pollution.aqius')
             .limit(1)
             .exec((error, data) => {
                 res.send(data)
             });
+        if (generate) {
+            await generateCSV(aqData);
+        }
     } catch (error) {
         console.log(error)
     }
@@ -27,6 +54,7 @@ const getWorstInMonth = async (req, res) => {
         //worst === false you get the best day with the lowest pollution
         const {start, end} = req.body
         const worst = req.body.worst === 'true';
+        const generate = req.body.genertateCSV === 'true';
         AQdata.find({
             'pollution.ts': {
                 $gte: start,
@@ -37,6 +65,9 @@ const getWorstInMonth = async (req, res) => {
             .exec((error, data) => {
                 res.send(data)
             });
+        if (generate) {
+            await generateCSV(aqData);
+        }
     } catch (error) {
         console.log(error)
     }
