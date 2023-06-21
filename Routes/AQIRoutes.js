1d643e5abd9f2fd85ee6c0b2506d6c109ac0646f
LukaP123456 <luka.prcic@icbtech.rs>
2023-06-21 11:57:43 +0200

    sort feature added to search

--- Routes/AQIRoutes.js
+++ Routes/AQIRoutes.js
@@ -7,8 +7,6 @@ const {
 router.get('/get/all', getAll)
 router.get('/get/x-of-alltime', xofAllTime)
 router.get('/get/x-in-time', getXInTime)
-// router.get('/search', search)
-//router.use and not router.delete or .put because I need this route to be both a delete and put route and.
 router.put('/:field', AddRemoveFavorite)
 router.delete('/:field', AddRemoveFavorite)
 router.use('/search', search)
--- app/Controllers/AQIController.js
+++ app/Controllers/AQIController.js
@@ -68,19 +68,27 @@ const getXInTime = async (req, res) => {
         const {start, end} = req.body
         const worst = req.body.worst === 'true';
         const generate = req.body.generateCSV === 'true';
-        AQdata.find({
-            'pollution.ts': {
+        const model_name_mapping = {
+            Hour: 'HourlyMeasurement',
+            Day: 'DailyMeasurement'
+        };
+        const model_name = model_name_mapping[req.body.model_name] || 'MonthlyMeasurement';
+        const Model = mongoose.model(model_name);
+        const results = await Model.find({
+            'time_stamp': {
                 $gte: start,
                 $lt: end
             }
-        }).sort(worst ? '-pollution.aqius' : 'pollution.aqius')
+        }).sort(worst ? {
+            'particular_matter_10.aqi_us_ranking': -1,
+            'particular_matter_25.aqi_us_ranking': -1
+        } : {'particular_matter_10.aqi_us_ranking': 1, 'particular_matter_25.aqi_us_ranking': 1})
             .limit(1)
-            .exec(async (error, data) => {
-                if (generate) {
-                    await generateCSV(data);
-                }
-                res.send(data)
-            });
+            .exec()
+        if (generate) {
+            await generateCSV(results);
+        }
+        res.send(results)
     } catch (error) {
         console.log(error)
     }
@@ -126,95 +134,15 @@ const search = async (req, res) => {
             }
         }
         console.log(mongo_query)
-        // const {
-        //     model_name,
-        //     name,
-        //     min_air_pressure,
-        //     max_air_pressure,
-        //     min_humidity,
-        //     max_humidity,
-        //     min_temperature,
-        //     max_temperature,
-        //     min_particular_matter_1,
-        //     max_particular_matter_1,
-        //     min_particular_matter_25_ranking,
-        //     max_particular_matter_25_ranking,
-        //     min_particular_matter_25_concentration,
-        //     max_particular_matter_25_concentration,
-        //     min_particular_matter_10_concentration,
-        //     max_particular_matter_10_concentration,
-        //     min_particular_matter_10_ranking,
-        //     max_particular_matter_10_ranking,
-        // } = req.query
-        // // Build the query object
-        // const mongo_query = {};
-        // if (name) {
-        //     mongo_query.name = {$regex: name, $options: 'i'};
-        // }
-        // if (min_air_pressure) {
-        //     mongo_query.air_pressure = {$gte: min_air_pressure};
-        // }
-        // if (max_air_pressure) {
-        //     mongo_query.air_pressure = {...mongo_query.air_pressure, $lte: max_air_pressure};
-        // }
-        // if (min_humidity) {
-        //     mongo_query.humidity = {$gte: min_humidity};
-        // }
-        // if (max_humidity) {
-        //     mongo_query.humidity = {...mongo_query.humidity, $lte: max_humidity};
-        // }
-        // if (min_temperature) {
-        //     mongo_query.temperature = {$gte: min_temperature};
-        // }
-        // if (max_temperature) {
-        //     mongo_query.temperature = {...mongo_query.temperature, $lte: max_temperature};
-        // }
-        // if (min_particular_matter_1) {
-        //     mongo_query.particular_matter_1 = {$gte: min_particular_matter_1};
-        // }
-        // if (max_particular_matter_1) {
-        //     mongo_query.particular_matter_1 = {...mongo_query.particular_matter_1, $lte: max_particular_matter_1};
-        // }
-        // if (min_particular_matter_25_ranking) {
-        //     mongo_query["particular_matter_25.aqi_us_ranking"] = {$gte: min_particular_matter_25_ranking};
-        // }
-        // if (max_particular_matter_25_ranking) {
-        //     mongo_query["particular_matter_25.aqi_us_ranking"] = {
-        //         ...mongo_query["particular_matter_25.aqi_us_ranking"],
-        //         $lte: max_particular_matter_25_ranking
-        //     };
-        // }
-        // if (min_particular_matter_25_concentration) {
-        //     mongo_query["particular_matter_25.concentration"] = {$gte: min_particular_matter_25_concentration};
-        // }
-        // if (max_particular_matter_25_concentration) {
-        //     mongo_query["particular_matter_25.concentration"] = {
-        //         ...mongo_query["particular_matter_25.concentration"],
-        //         $lte: max_particular_matter_25_concentration
-        //     };
-        // }
-        // if (min_particular_matter_10_concentration) {
-        //     mongo_query["particular_matter_10.concentration"] = {$gte: min_particular_matter_10_concentration};
-        // }
-        // if (max_particular_matter_10_concentration) {
-        //     mongo_query["particular_matter_10.concentration"] = {
-        //         ...mongo_query["particular_matter_10.concentration"],
-        //         $lte: max_particular_matter_10_concentration
-        //     };
-        // }
-        // if (min_particular_matter_10_ranking) {
-        //     mongo_query["particular_matter_10.aqi_us_ranking"] = {$gte: min_particular_matter_10_ranking};
-        // }
-        // if (max_particular_matter_10_ranking) {
-        //     mongo_query["particular_matter_10.aqi_us_ranking"] = {
-        //         ...mongo_query["particular_matter_10.aqi_us_ranking"],
-        //         $lte: max_particular_matter_10_ranking
-        //     };
-        // }
+        // Add sort option
+        const sort = {};
+        if (req.query.sort_by && req.query.order) {
+            sort[req.query.sort_by] = req.query.order === 'asc' ? 1 : -1;
+        }
         // Perform the search
         const Model = mongoose.model(model_name);
         // Use the Model to perform a search
-        const results = await Model.find(mongo_query);
+        const results = await Model.find(mongo_query).sort(sort);
         res.send(results);
     } catch (error) {
         console.log(error)
