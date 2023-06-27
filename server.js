8814ffbd28d4f7c35cd28b0eef42808b018c9510
LukaP123456 <luka.prcic@icbtech.rs>
2023-06-27 09:42:32 +0200

    commented out the test cron jobs

--- server.js
+++ server.js
@@ -128,18 +128,19 @@ const start = async () => {
 
 start();
 //CRON JOBS
-cron.schedule('0 * * * *', () => {
-    get_hourly_data(true)
-    console.log('Running every hour');
-});
-cron.schedule('5 * * * *', () => {
-    get_daily_data(true)
-    console.log('Running every hour');
-});
-cron.schedule('10 * * * *', () => {
-    get_monthly_data(true)
-    console.log('Running every hour');
-});
+//Test cron jobs
+// cron.schedule('0 * * * *', () => {
+//     get_hourly_data(true)
+//     console.log('Running every hour');
+// });
+// cron.schedule('5 * * * *', () => {
+//     get_daily_data(true)
+//     console.log('Running every hour');
+// });
+// cron.schedule('10 * * * *', () => {
+//     get_monthly_data(true)
+//     console.log('Running every hour');
+// });
 
 cron.schedule('0 0 */2 * *', () => {
     get_hourly_data()
